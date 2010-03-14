145188b26d2a7c08571af7fbb001eec7065abb60
Jason Gladish <jason@expectedbehavior.com>
2010-03-14 13:30:00 -0400

    first passing test, and fixing dumb issue with option parsing

--- test/test_web_server_config_generator.rb
+++ test/test_web_server_config_generator.rb
@@ -3,9 +3,11 @@ require File.dirname(__FILE__) + '/test_helper.rb'
 class TestWebServerConfigGenerator < Test::Unit::TestCase
 
   def setup
+    $CMD = File.join(File.dirname(__FILE__), "..", "bin", "web_server_setup")
   end
   
-  def test_truth
-    assert true
+  def test_help
+    cmd = "#{$CMD} -h"
+    assert_match /Usage/, `#{cmd}`
   end
 end
