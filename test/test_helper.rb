c51fa923f670b0e0a79e669a5c2451f3c44c2a4a
Jason Gladish <jason@expectedbehavior.com>
2010-03-14 14:31:48 -0400

    adding test for specifying config files location

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,4 @@
 require 'stringio'
 require 'test/unit'
+require 'fileutils'
 require File.dirname(__FILE__) + '/../lib/web_server_config_generator'
--- test/test_web_server_config_generator.rb
+++ test/test_web_server_config_generator.rb
@@ -4,10 +4,26 @@ class TestWebServerConfigGenerator < Test::Unit::TestCase
 
   def setup
     $CMD = File.join(File.dirname(__FILE__), "..", "bin", "web_server_setup")
+    $EXAMPLE_APPS = [
+                     $STAND_ALONE_APP = File.join(File.dirname(__FILE__), "test_apps", "stand_alone_app"),
+                     $SUB_URI_APP_FOO = File.join(File.dirname(__FILE__), "test_apps", "sub_uri_app_foo"),
+                     $SUB_URI_APP_BAR = File.join(File.dirname(__FILE__), "test_apps", "sub_uri_app_bar"),
+                    ]
   end
   
   def test_help
     cmd = "#{$CMD} -h"
     assert_match /Usage/, `#{cmd}`
+  end
+  
+  def test_specifying_config_location
+    loc = File.join(File.dirname(__FILE__), "test_generated_config_files")
+    FileUtils.rm_r loc if File.exist? loc
+    assert !File.exist?(loc)
+    cmd = "#{$CMD} --no-add-hosts --no-restart-nginx -l #{loc} #{$STAND_ALONE_APP}"
+    `#{cmd}`
+    assert File.exist?(loc)
+    assert File.exist?(File.join(loc, "links"))
+    assert File.exist?(File.join(loc, "vhost"))
   end
 end
